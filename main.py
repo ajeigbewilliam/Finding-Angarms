066216784a30d2d85feb1cc5789e3b276411f156
William Ajeigbe <105397898+ajeigbewilliam@users.noreply.github.com>
2022-05-27 15:47:53 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+def find_anagrams(s1, s2):
+    #the sorted strings are checked
+    if(sorted(s1)== sorted(s2)):
+        print("True")
+    else:
+        print("False")
+        find_anagram("brainy", "binary")
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+
+    return True
+
